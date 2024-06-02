7aba60dd98cd7369abc07466e2a88d15abf8087c
Ignacio Barrientos <ifbarrientos@outlook.com>
2024-06-02 13:44:07 -0300

    Se arregló el regex pero sigue el seg fault

--- programa.c
+++ programa.c
@@ -10,13 +10,9 @@ extern int resta(int a, int b);
 extern int multiply(int a, int b);
 extern int divide(int a, int b);
 
-int nro1, nro2;
-char oper_texto[4];
-char oper;
-char str[LONG_MAX];
-int resultado;
+int nro1, nro2, resultado;
+char oper_texto[4], oper, str[LONG_MAX];
 
-//TODO
 int calcularOperacion(int operando1, char operador, int operando2){
     if (operador == '+'){
         resultado = suma(operando1, operando2);
@@ -31,6 +27,10 @@ int calcularOperacion(int operando1, char operador, int operando2){
         printf("El resultado es: %d\n", resultado);
         return resultado;
     } else if (operador == '/'){
+        if (operando2 == 0){
+            fprintf(stderr, "No se puede dividir por cero");
+            return 0;
+        }
         resultado = divide(operando1, operando2);
         printf("El resultado es: %d\n", resultado);
         return resultado;
@@ -52,10 +52,6 @@ void identificarNrosYDelim(){
         strcpy(oper_texto, " * ");
         oper = '*';
     } else if (strstr(str, " / ") != NULL) {
-        if (nro2 == 0){
-            fprintf(stderr, "No se puede dividir por cero.\n");
-            return;
-        }
         printf("División detectada!\n");
         strcpy(oper_texto, " / ");
         oper = '/';
@@ -80,22 +76,29 @@ void identificarNrosYDelim(){
         i++;
         token = strtok(NULL, oper_texto);
     }
-    printf("Nro1 = %d, Nro2 = %d, Operador =%c\n", nro1,nro2,oper);
+    printf("Nro1 = %d, Nro2 = %d, Operador = %c\n", nro1,nro2,oper);
 }
 
 void leer_pregunta(){
     printf("Por favor, ingrese una operación matemática con espacios entre los numeros y el operando:\n");
+    const char *patron = "^-?[[:digit:]]{1,10}[[:space:]][+\\*\\/-][[:space:]]-?[[:digit:]]{1,10}$";
     regex_t regex;
     int ret;
-    const char *patron = "-?[[:digit:]]{1,10}[[:space:]][\\+\\*\\/\\-][[:space:]]-?[[:digit:]]{1,10}";
-    ret = regcomp(&regex, patron, REG_EXTENDED);
-    char msgbuf[LONG_MAX];
-    char *p;
 
-    fgets(str, LONG_MAX, stdin); //Hace el input
+    ret = regcomp(&regex, patron, REG_EXTENDED);
+
+    if (fgets(str, LONG_MAX, stdin) != NULL) { //Hace el input
+        size_t len = strlen(str);
+        if (len > 0 && str[len-1] == '\n')
+            str[len-1] = '\0';
+    } else {
+        fprintf(stderr, "Hubo un error al leer el input\n");
+        return;
+    }
 
     if (ret){
         fprintf(stderr, "No se pudo compilar el regex\n");
+        return;
     }
 
     ret = regexec(&regex, str, 0, NULL, 0);
@@ -105,14 +108,16 @@ void leer_pregunta(){
         puts("Lo siento, mis respuestas son limitadas.\nAseguráte de escribir la operación como '1 + 2' por ejemplo.");
         return;
     } else {
+        char msgbuf[LONG_MAX];
         regerror(ret, &regex, msgbuf, sizeof(msgbuf));
         fprintf(stderr, "Fallo el matcheo de regex: %s\n", msgbuf);
+        return;
     }
 
     regfree(&regex);
 
     identificarNrosYDelim();
-    printf("oper: %d", oper);
+    //printf("oper: %d\n", oper);
     calcularOperacion(nro1,oper,nro2);
 }
 
